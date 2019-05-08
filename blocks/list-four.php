0758acd3d2c49df193a8cdda0ed9c1dff414c13d
Vladimir Samojlov <v-moi@ya.ru>
2019-05-09 00:46:06 +0300

    add pravki

--- blocks/list-four.php
+++ blocks/list-four.php
@@ -21,7 +21,7 @@
 						<div class="animate-puk" data-effect="fade-up" data-delay="0.5s">
 							<div class="list-four__item">
 								<svg class="t508__checkmark" style="width:40px; height:40px;" fill="#1f5bff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.8 31.8"><path d="M.9 14.5C5.2 19.4 9.8 24 14.1 29c.4.5 1.3.3 1.6-.2 5.1-8.4 10.1-16.8 15-25.4.7-1.2-.5-2.1-1.3-.9C19.3 18 19 18.4 13.9 26.7c.5-.1 1.1-.1 1.6-.2-4.4-4.8-8.9-8.6-13.9-12.8-.4-.4-1.1.3-.7.8z"></path></svg>
-								<span>Подбор схемы лечения</span>
+								<span>Более 2000 благодарны за оказанную помощь</span>
 							</div>
 						</div>
 					</div>
@@ -45,7 +45,7 @@
 						<div class="animate-puk" data-effect="fade-up" data-delay="1.4s">
 							<div class="list-four__item">
 								<svg class="t508__checkmark" style="width:40px; height:40px;" fill="#1f5bff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 31.8 31.8"><path d="M.9 14.5C5.2 19.4 9.8 24 14.1 29c.4.5 1.3.3 1.6-.2 5.1-8.4 10.1-16.8 15-25.4.7-1.2-.5-2.1-1.3-.9C19.3 18 19 18.4 13.9 26.7c.5-.1 1.1-.1 1.6-.2-4.4-4.8-8.9-8.6-13.9-12.8-.4-.4-1.1.3-.7.8z"></path></svg>
-								<span>Более 2000 благодарны за оказанную помощь</span>
+								<span>Подбор схемы лечения</span>
 							</div>
 						</div>
 					</div>
