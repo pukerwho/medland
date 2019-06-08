6a3ccb9e4dc605485ee126e6c846c38773739776
Vladimir Samojlov <v-moi@ya.ru>
2019-06-08 22:06:54 +0300

    add google analitycs

--- header.php
+++ header.php
@@ -17,6 +17,15 @@
 
     wp_head();
 	?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140429702-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-140429702-2');
+  </script>
 </head>
 <body <?php echo body_class(); ?>>
   <!-- <div class="preloader"></div> -->
